854cbb00869ea6c3fc56a277ada89936aead2d47
Marcelo Caetano <9254896+marcelo-caetano@users.noreply.github.com>
2019-10-12 15:34:27 +0100

    Updated sinusoidal model

--- run_smt.m
+++ run_smt.m
@@ -4,10 +4,12 @@
 % INPUT PARAMETERS
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% Path relative to SMT folder
-sourcePath = fullfile('.','audio','Accordion_C#3_f.wav');
+% Path relative to CURREXFILE
+% sourcePath = fullfile('.','audio','Accordion_C#3_f.wav');
+sourcePath = fullfile(pwd,'MCaetano','Production','sound-morphing','audio','Accordion_C#3_f.wav');
 
-targetPath = fullfile('.','audio','Tuba_oV_hA_2-120_ff_C3.wav');
+% targetPath = fullfile('.','audio','Tuba_oV_hA_2-120_ff_C3.wav');
+targetPath = fullfile(pwd,'MCaetano','Production','sound-morphing','audio','Tuba_oV_hA_2-120_ff_C3.wav');
 
 % Morphing factor
 morphFactor = 0.5;
